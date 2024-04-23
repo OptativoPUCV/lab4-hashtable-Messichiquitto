bae0185d8d25446fae6d8db1f38e9fec62296822
Messichiquitto <163443627+Messichiquitto@users.noreply.github.com>
2024-04-23 23:45:56 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -115,11 +115,28 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
-    return NULL;
+  if(map==NULL) return NULL;
+  for(long i=0;i<map->capacity;i++)
+    {
+      if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL)
+      {
+        map->current=i;
+        return map->buckets[i];
+      }
+    }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
-
-    return NULL;
+  if(map==NULL) return NULL;
+  if(map->current==-1) return NULL;
+  for(long i=map->current+1;i<map->capacity;i++)
+    {
+      if(map->buckets[i]!=NULL && map->buckets[i]->key!=NULL)
+      {
+        map->current=i;
+        return map->buckets[i];
+      }
+    }
+  return NULL;
 }
